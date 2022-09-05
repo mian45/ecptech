60fa13eaa0a5e2f8335eddcbec34e460fb30acbe
Mirza Safeer Baig <safeer.baig@wadic.dev>
2022-09-06 04:06:43 +0500

    fix: fix issues

--- resources/js/email-remainder/index.js
+++ resources/js/email-remainder/index.js
@@ -1,88 +1,149 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
+import AuthServices from '../services'
+
 import './style.scss'
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import ReactTags from "react-tag-autocomplete";
-import {EditorState} from "draft-js";
-import {Editor} from "react-draft-wysiwyg"
+
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Dropdown, Menu, message, Space, Tooltip } from 'antd';
+
+import { Tag } from 'antd';
+
+import { Editor } from "react-draft-wysiwyg"
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-const EmailRemainder = () => {
-    const [editorState, setEditorState] = useState()
-    const [subject, setSubject] = useState('')
-    const [emailType, setEmailType] = useState('')
-    const [tags, setTags] = useState([{id: 1, name: "English"}])
-    const [suggestions, setSuggestions] = useState([
-        {id: 1, name: "English"},
-        {id: 2, name: "Spain"},
-        {id: 3, name: "Italy"}
-    ])
-    const [sentDate, setSentDate] = useState('')
-    const [sentTime, setSentTime] = useState('')
-    const [timeZone, setTimeZone] = useState('')
 
+const EmailRemainder = (props) => {
+    const [editorState, setEditorState] = useState('')
 
+    const handleMenuClick = (e) => {
+        message.info('Click on menu item.');
+    };
 
-    const handleDelete = (i) => {
-        const tags = tags.slice(0);
-        tags.splice(i, 1);
-        setTags({tags});
+    const onEditorStateChange = () => {
+        setEditorState(editorState)
     }
 
-    const handleAddition = (tag) => {
-        const tags = [].concat(tags, tag);
-        setTags({tags});
+    const handleClick = () => {
+        props.dispatch(AuthServices.activeSetting(props.isActiveSettingState-1))
+
     }
-    const options = [
-        'one', 'two', 'three'
-    ];
+
+    const menu = (
+        <Menu
+            onClick={handleMenuClick}
+            items={[
+                {
+                    label: 'Reminder',
+                    key: '1',
+                    icon: <UserOutlined />,
+                },
+                {
+                    label: '2nd menu item',
+                    key: '2',
+                    icon: <UserOutlined />,
+                },
+            ]}
+        />
+    );
+
+    const date = (
+        <Menu
+            onClick={handleMenuClick}
+            items={[
+                {
+                    label: 'Reminder',
+                    key: '1',
+                    icon: <UserOutlined />,
+                },
+                {
+                    label: '2nd menu item',
+                    key: '2',
+                    icon: <UserOutlined />,
+                },
+            ]}
+        />
+    );
+
+    const time = (
+        <Menu
+            onClick={handleMenuClick}
+            items={[
+                {
+                    label: 'Reminder',
+                    key: '1',
+                    icon: <UserOutlined />,
+                },
+                {
+                    label: '2nd menu item',
+                    key: '2',
+                    icon: <UserOutlined />,
+                },
+            ]}
+        />
+    );
+
+    const timeZone = (
+        <Menu
+            onClick={handleMenuClick}
+            items={[
+                {
+                    label: 'Reminder',
+                    key: '1',
+                    icon: <UserOutlined />,
+                },
+                {
+                    label: '2nd menu item',
+                    key: '2',
+                    icon: <UserOutlined />,
+                },
+            ]}
+        />
+    );
     const handleSubmit = (event) => {
         event.preventDefault();
 
 
     }
-
-    const onEditorStateChange = (editorState) => {
-        setEditorState(editorState)
-    };
     return (
         <form onSubmit={handleSubmit}>
             <div className='email-remainder'>
                 <p className='email-remainder_heading'>Add New</p>
 
                 <div>
-                    <div className='email-remainder_arrow-section'>
-                        <img src={'/arrow-back.svg'} alt='arrow'/>
+                    <div onClick={handleClick} className='email-remainder_arrow-section'>
+                        <img src={'/arrow-back.svg'} alt='arrow' />
                         <p className='email-remainder_arrow-section_back'>Back</p>
                     </div>
                     <div className='email-remainder_input-sections'>
                         <div className='email-remainder_input-sections_input-section'>
                             <p>Email Type</p>
-                            <Autocomplete
-                                onChange={(event, value) => setEmailType(value)}
-                                disablePortal
-                                id="combo-box-demo"
-                                options={options}
-                                sx={{width: 300}}
-                                renderInput={(params) => <TextField {...params} label="Reminder"/>}
-                            />
+                            <Dropdown className='dropdown-email' overlay={menu}>
+                                <Button>
+                                    <Space>
+                                        Reminder
+                                        <DownOutlined />
+                                    </Space>
+                                </Button>
+                            </Dropdown>
                         </div>
                         <div className='email-remainder_input-sections_input-section'>
                             <p>Send to</p>
-                            <ReactTags
-                                tags={tags}
-                                suggestions={suggestions}
-                                handleDelete={handleDelete}
-                                handleAddition={handleAddition}
-                                allowNew
-                                minQueryLength={1}
-                            />
+                            <Dropdown className='dropdown-email' overlay={menu}>
+                                <Button>
+                                    <Space>
+                                        <Tag className='tags'>All customer</Tag>
+                                        <DownOutlined />
+                                    </Space>
+                                </Button>
+                            </Dropdown>
                         </div>
                         <div className='email-remainder_input-sections_input-section'>
                             <p>Subject</p>
                             <input className='email-remainder_input-sections_input-section_input' onChange={(e) => {
                                 setSubject(e.target.value)
-                            }} type={'text'} required/>
+                            }} type={'text'} required />
                         </div>
 
                         <Editor
@@ -95,36 +156,36 @@ const EmailRemainder = () => {
                         <p className='email-remainder_schedule'>Schedule</p>
                         <div className='email-remainder_input-sections_input-section'>
                             <p>Send date</p>
-                            <Autocomplete
-                                onChange={(event, value) => setSentDate(value)}
-                                disablePortal
-                                id="combo-box-demo"
-                                options={options}
-                                sx={{width: 300}}
-                                renderInput={(params) => <TextField {...params} label="2 days after invoice sent"/>}
-                            />
+                            <Dropdown className='dropdown-email' overlay={date}>
+                                <Button>
+                                    <Space>
+                                        2 days after invoice sent
+                                        <DownOutlined />
+                                    </Space>
+                                </Button>
+                            </Dropdown>
                         </div>
                         <div className='email-remainder_input-sections_input-section'>
                             <p>Send Time</p>
-                            <Autocomplete
-                                disablePorta
-                                onChange={(event, value) => setSentTime(value)} l
-                                id="combo-box-demo"
-                                options={options}
-                                sx={{width: 300}}
-                                renderInput={(params) => <TextField {...params} label="9:00 AM"/>}
-                            />
+                            <Dropdown className='dropdown-email' overlay={time}>
+                                <Button>
+                                    <Space>
+                                        9:00 AM
+                                        <DownOutlined />
+                                    </Space>
+                                </Button>
+                            </Dropdown>
                         </div>
                         <div className='email-remainder_input-sections_input-section'>
                             <p>Time Zone</p>
-                            <Autocomplete
-                                onChange={(event, value) => setTimeZone(value)}
-                                disablePortal
-                                id="combo-box-demo"
-                                options={options}
-                                sx={{width: 300}}
-                                renderInput={(params) => <TextField {...params} label="Washington, DC, USA (GMT-4)"/>}
-                            />
+                            <Dropdown className='dropdown-email' overlay={timeZone}>
+                                <Button>
+                                    <Space>
+                                        Washington, DC, USA (GMT-4)
+                                        <DownOutlined />
+                                    </Space>
+                                </Button>
+                            </Dropdown>
                         </div>
                         <button className='email-remainder_save-button'>Save</button>
                     </div>
@@ -134,4 +195,15 @@ const EmailRemainder = () => {
     )
 }
 
-export default EmailRemainder
+EmailRemainder.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    isActiveSettingState: PropTypes.number.isRequired,
+
+};
+
+const mapStateToProps = (state) => ({
+
+    isActiveSettingState: state.Auth.isActiveSettingState
+});
+
+export default  connect(mapStateToProps) (EmailRemainder)
