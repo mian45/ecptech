180d0aa0d4553d590449da959bb073016bea7b4f
Ali Raza <ali.raza@wadic.dev>
2022-09-06 00:01:06 +0000

    ECP-117-added-profile-color-schema

--- resources/js/components/profile/Profile.js
+++ resources/js/components/profile/Profile.js
@@ -1,23 +1,27 @@
-import React from "react"
-import "./profil.scss"
-import PhotoUpload from "./PhotoUpload.js"
-const Profile=()=>{
-    return(
-        <div className='profile'>
-            <div className='busniss-info'>
-                <p className='busniess-label'>Business Information</p>
-                <PhotoUpload/>
+import React from "react";
+import "./profil.scss";
+import PhotoUpload from "./PhotoUpload.js";
+import ColorTheme from "../colorTheme";
+
+const Profile = () => {
+    return (
+        <div className="profile">
+            <div className="busniss-info">
+                <p className="busniess-label">Business Information</p>
+                <PhotoUpload />
                 <div>
-                    <label>
-                        Business Name
-                    </label>
-                    <br/>
-                    <input type="text" placeholder="Enter business name" className="business-name"/>
+                    <label>Business Name</label>
+                    <br />
+                    <input
+                        type="text"
+                        placeholder="Enter business name"
+                        className="business-name"
+                    />
                 </div>
-
             </div>
+            <ColorTheme />
         </div>
-    )
-}
+    );
+};
 
-export default Profile
+export default Profile;
