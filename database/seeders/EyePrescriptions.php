99fa1b41a4e794b4a4857544f3b14de8def305fa
Muhammad Usman <muhammad.usman@wadic.dev>
2022-09-10 05:13:31 +0000

    migration and seeders updated

--- database/seeders/EyePrescriptions.php
+++ database/seeders/EyePrescriptions.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class Eye_prescriptions extends Seeder
+class EyePrescriptions extends Seeder
 {
     /**
      * Run the database seeds.
--- database/seeders/Transactions.php
+++ database/seeders/Transactions.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class transactions extends Seeder
+class Transactions extends Seeder
 {
     /**
      * Run the database seeds.
