8c8c3ece17a5b3c74dc6c3f9402778b08cb84f99
Ali Raza <ali.raza@wadic.dev>
2022-09-10 02:16:07 +0000

    api implementation

--- resources/js/Invoices/index.js
+++ resources/js/Invoices/index.js
@@ -1,24 +1,70 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import InvoicesForm from "../components/invoiceForm";
 import classes from "./styles.module.scss";
 import { Formik } from "formik";
 import { InvoiceInitialValues } from "./data/initialValues";
 import InvoiceValidation from "./data/validations";
 import InvoicesTable from "../components/invoiceTable";
+import axios from "axios";
+import { connect } from "react-redux";
+import { getMonth, getYear, getDate } from "date-fns";
 
-const Invoices = () => {
+const Invoices = ({ userId }) => {
     const [isSearched, setIsSearched] = useState(false);
+    const [tableData, setTableData] = useState([]);
+
+    useEffect(() => {
+        const getAllInvoices = async () => {
+            const res = await axios.get(
+                "http://dev.usman-ecptech.wadic.net/api/get_invoices",
+                { params: { user_id: userId } }
+            );
+            mapInvoicesData(res.data?.data);
+        };
+        getAllInvoices();
+    }, []);
+
     const handleClick = (values) => {
         console.log("values", values);
     };
-    const handleSearch = (values) => {
+    const handleSearch = async (values) => {
         try {
             setIsSearched(true);
-            console.log("searched values", values);
+            const invoiceObject = {
+                first_name: values?.firstName,
+                last_name: values?.lastName,
+                user_id: userId,
+                email: values?.email,
+                phone_number: values?.phoneNo,
+                dob: values?.dob,
+            };
+
+            const res = await axios.post(
+                "http://dev.usman-ecptech.wadic.net/api/search_invoices",
+                invoiceObject
+            );
+            mapInvoicesData(res.data?.data);
         } catch (err) {
             setIsSearched(false);
             console.log("error while search", err);
         }
+    };
+    const mapInvoicesData = (data) => {
+        const mappedInvoices = data?.map((invoice) => {
+            const createdDate = new Date(invoice?.created_at);
+            const date = `${getYear(createdDate)}-${
+                getMonth(createdDate) + 1
+            }-${getDate(createdDate)}`;
+            return {
+                invoice: invoice?.name,
+                customerName: invoice?.customer_name,
+                email: invoice?.customer_email,
+                date: date,
+                price: invoice?.amount,
+                status: invoice?.status,
+            };
+        });
+        setTableData([...mappedInvoices]);
     };
     return (
         <div className={classes["container"]}>
@@ -43,8 +89,11 @@ const Invoices = () => {
                     );
                 }}
             </Formik>
-            <InvoicesTable />
+            <InvoicesTable data={tableData} />
         </div>
     );
 };
-export default Invoices;
+const mapStateToProps = (state) => ({
+    userId: state.Auth.user?.id,
+});
+export default connect(mapStateToProps)(Invoices);
--- resources/js/components/invoiceTable/index.js
+++ resources/js/components/invoiceTable/index.js
@@ -4,14 +4,14 @@ import { INVOICE_DATA } from "./components/invoicesTableBody/data/invoiceData";
 import InvoicesTableHeader from "./components/invoicesTableHeader";
 import classes from "./styles.module.scss";
 
-const InvoicesTable = () => {
+const InvoicesTable = ({ data }) => {
     return (
         <table className={classes["table"]}>
             <thead className={classes["table-header"]}>
                 <InvoicesTableHeader />
             </thead>
             <tbody>
-                {INVOICE_DATA.map((invoice, index) => {
+                {data?.map((invoice, index) => {
                     return <InvoicesTableBodySlot data={invoice} key={index} />;
                 })}
             </tbody>
