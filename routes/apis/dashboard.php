b3bffd158f583e90ceaf3f12ee7db180b836b0a6
Hasnain Abid Khanzada <hasnain.abid@wadic.dev>
2022-09-16 20:13:44 +0000

    Team Progress API

--- routes/apis/dashboard.php
+++ routes/apis/dashboard.php
@@ -6,4 +6,5 @@ use App\Http\Controllers\Api\DashboardController;
 Route::post('invoice-summmary', [DashboardController::class, 'getInvoiceSummmary']);
 Route::post('invoice-stats', [DashboardController::class, 'getInvoiceStats']);
 Route::post('team-progress', [DashboardController::class, 'getTeamProgress']);
+Route::post('team-performance', [DashboardController::class, 'getTeamPerformance']);
 
