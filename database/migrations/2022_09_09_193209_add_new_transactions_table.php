92b958351e551a3aae3202e8e74ba41def2980a8
Muhammad Usman <muhammad.usman@wadic.dev>
2022-09-17 01:53:16 +0000

    migration code updated

--- database/migrations/2022_09_09_193209_add_new_transactions_table.php
+++ database/migrations/2022_09_09_193209_add_new_transactions_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('invoice_id');
+            $table->integer('user_id');
+            $table->integer('invoice_id');
             $table->integer('amount');
             $table->string('status');
             $table->string('customer_name');
--- database/migrations/2022_09_13_163339_create_products_table.php
+++ database/migrations/2022_09_13_163339_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->integer('user_id');
             $table->string('name', 50);
             $table->integer('price');
             $table->timestamps();
