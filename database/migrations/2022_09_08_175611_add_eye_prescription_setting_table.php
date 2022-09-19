db45b3e849a7605f680c95293f5bbdb4066d54d7
Muhammad Usman <muhammad.usman@wadic.dev>
2022-09-19 19:05:37 +0000

    prescription migrationupdated

--- database/migrations/2022_09_08_175611_add_eye_prescription_setting_table.php
+++ database/migrations/2022_09_08_175611_add_eye_prescription_setting_table.php
@@ -13,17 +13,20 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('eye_prescription_setting', function (Blueprint $table) {
+        Schema::create('prescriptions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->decimal('sphere_from', 5, 2);
             $table->decimal('sphere_to', 5, 2);
             $table->decimal('cylinder_from', 5, 2);
             $table->decimal('cylinder_to', 5, 2);
-            $table->integer('user_id')->nullable();
+            $table->bigInteger('user_id')->unsigned();
             $table->softDeletes();
             $table->timestamps();
         });       
+        Schema::table('prescriptions', function($table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
     }
 
     /**
@@ -33,6 +36,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('eye_prescription_setting');
+        Schema::dropIfExists('prescriptions');
     }
 };
