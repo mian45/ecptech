4cb25441a1e6ed431c9d5b9e1e3f4cb360492028
Hasnain Abid Khanzada <hasnain.abid@wadic.dev>
2022-09-20 21:02:32 +0000

    Migrations for Calculater

---
+++ database/migrations/2022_09_20_192729_create_options_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('options', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('type');
+            $table->string('code');
+            $table->decimal('price',10,2);
+            $table->unsignedBigInteger('question_id');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('question_id')->references('id')->on('questions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('options');
+    }
+};
