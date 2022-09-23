9b1503f4f671d69a114232a893ff58db9eb976e2
Hasnain Abid Khanzada <hasnain.abid@wadic.dev>
2022-09-23 17:00:13 +0000

    Files Renamed

--- database/seeders/UserLenseSettingsTableSeeder.php
+++ database/seeders/UserLenseSettingsTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class ClientLenseSettingsTableSeeder extends Seeder
+class UserLenseSettingsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
