3a6624b098c3445102d497225637f7c8bd76a305
waseem_mushtaq <waseem.mushtaq@wadic.dev>
2022-09-23 23:27:48 +0000

    edit discount api

---
+++ routes/apis/discount.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\Api\DiscountController;
+
+
+Route::get('get-discount', [DiscountController::class, 'getDiscount']);
+Route::post('add-discount', [DiscountController::class, 'addDiscount']);
+Route::post('edit-discount', [DiscountController::class, 'EditDiscount']);
+Route::post('delete-discount', [DiscountController::class, 'deleteDiscount']);
