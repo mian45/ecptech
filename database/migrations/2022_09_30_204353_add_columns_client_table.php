c1f4de3b9c3c17fce7910c28e8bdecddc22a7b42
Hasnain Abid Khanzada <hasnain.abid@wadic.dev>
2022-09-30 21:12:26 +0000

    Columns Added for Payment

---
+++ database/migrations/2022_09_30_204353_add_columns_client_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            $table->string('card_no');
+            $table->string('card_name');
+            $table->string('card_expiry');
+            $table->string('ccv')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            //
+        });
+    }
+};
