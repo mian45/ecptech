e8a95e675a8144f2a5e5d16527d384ffc579791b
waseem_mushtaq <waseem.mushtaq@wadic.dev>
2022-10-04 21:20:02 +0000

    resolve conflicts

--- routes/apis/setting.php
+++ routes/apis/setting.php
@@ -3,9 +3,5 @@
 use App\Http\Controllers\Api\SettingController;
 
 
-<<<<<<< HEAD
-Route::get('get-lense-features-brands', [SettingController::class, 'getLenseFeaturesBrands']);
-=======
 Route::get('lense-material-settings', [SettingController::class, 'getLenseMaterial']);
 Route::post('add-lense-material-setting', [SettingController::class, 'addLenseMaterial']);
->>>>>>> version-2.0
