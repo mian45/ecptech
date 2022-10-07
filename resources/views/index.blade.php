3a09489293035b688a441260d89821dc301e2b0d
itwadic <saad.alvi@wadic.dev>
2022-10-07 21:09:22 +0500

    fix for login not in center

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,9 +11,9 @@
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     </head>
     <body>
-        <div id="page" class="d-flex flex-column">
-            <div id="app"></div>
-        </div>
+    
+            <div id="app" style="height:100%;width:100%"></div>
+
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
