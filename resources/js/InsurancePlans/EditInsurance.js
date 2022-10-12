5c21e06890c1a89fc46258db7f4a44aca296751e
shahid.manzoor <shahid.manzoor@wadic.dev>
2022-10-12 19:42:31 +0000

    Insurance Plan API integration

--- resources/js/InsurancePlans/EditInsurance.js
+++ resources/js/InsurancePlans/EditInsurance.js
@@ -2,10 +2,42 @@ import React from 'react';
 import { Switch } from 'antd';
 import './style.scss'
 import { Checkbox } from 'antd';
-import { useHistory } from 'react-router';
-
+import { useHistory,useParams } from 'react-router';
+import axios from 'axios';
 function EditInsurance(props) {
+
+  const [selectedRow,setSelectedRow]=React.useState([])
+  const [isChecked,setIsChecked]=React.useState(false)
+  const [isCheckBox,setIsCheckBox]=React.useState(false)
   const history=useHistory()
+const params=useParams();
+
+//for editing 
+React.useEffect(()=>{
+
+  axios.get(process.env.MIX_REACT_APP_URL+`/api/get-client-plan-questions?visionPlanId=${params?.id}`).then((res)=>{
+    
+    setSelectedRow(res.data?.data)
+  }).catch((error)=>console.log({error}))
+  
+  },[])
+
+
+
+//for toggle switch
+
+const handleSwitch=(checked)=>{
+  console.log({checked})
+setIsChecked(checked)
+}
+
+//for toggle checkbox
+
+const handleCheckbox=(e)=>{
+  console.log(e.target.checked)
+  setIsCheckBox(e.target.checked)
+}
+
     const label = { inputProps: { 'aria-label': 'Switch demo' } }
     return (
         <div className="root-container">
@@ -25,46 +57,21 @@ function EditInsurance(props) {
                 </div>
                 <div className='other-setting_section-first' >
                 {/* component to be used in map */}
-                <div className='other-setting_section-first_switches-switch-edit'>
-                          <Checkbox  >Select Vision Plan</Checkbox>
+            {
+                selectedRow?.length>0 && selectedRow.map((item)=>{
+                    return(
+                      <div className='other-setting_section-first_switches-switch-edit' key={item?.id}>
+                          <Checkbox >{item?.title}</Checkbox>
                             
-                            <Switch {...label} />
-                            
-                </div>
-                <div className='other-setting_section-first_switches-switch-edit'>
-                          <Checkbox  >Select Vision Plan</Checkbox>
-                            
-                            <Switch {...label} />
-                            
-                </div>
-                <div className='other-setting_section-first_switches-switch-edit'>
-                          <Checkbox  >Select Vision Plan</Checkbox>
-                            
-                            <Switch {...label} />
-                            
-                </div>
-                <div className='other-setting_section-first_switches-switch-edit'>
-                          <Checkbox  >Select Vision Plan</Checkbox>
-                            
-                            <Switch {...label} />
-                            
-                </div>
-                <div className='other-setting_section-first_switches-switch-edit'>
-                          <Checkbox  >Select Vision Plan</Checkbox>
-                            
-                            <Switch {...label} />
-                            
-                </div>
-                <div className='other-setting_section-first_switches-switch-edit'>
-                          <Checkbox  >Select Vision Plan</Checkbox>
-                            
-                            <Switch {...label} />
-                            
-                </div>
+                            <Switch {...label}  defaultChecked={item?.optional===0?false:true ||isChecked} onChange={handleSwitch}/>
+                            </div>
+                        )
+  }) 
+}
                 </div>
                
             </div>
-            <button
+                                <button
                                     type="submit"
                                     className={"button"}
                                 >
--- resources/js/InsurancePlans/index.js
+++ resources/js/InsurancePlans/index.js
@@ -3,17 +3,54 @@ import { Switch } from "antd";
 import "./style.scss";
 import bellIcon from "../../images/edit.png";
 import { useHistory } from "react-router";
+import axios from "axios";
+
+
 const InsurancePlans = () => {
+
+const [getData,setGetData]=React.useState([])
+const [isChecked,setIsChecked]=React.useState(false)
+
+
     const label = { inputProps: { "aria-label": "Switch demo" } };
     const history = useHistory();
+
+// to fetch data from api
+
+React.useEffect(()=>{
+
+axios.get(process.env.MIX_REACT_APP_URL+"/api/get-client-vision-plans").then((res)=>{
+  
+    setGetData(res.data?.data)
+}).catch((error)=>console.log({error}))
+
+},[])
+    
+
+//for toggle switch
+
+const handleSwitch=(checked)=>{
+    console.log({checked})
+setIsChecked(checked)
+}
+    
+
+
     return (
         <div className="other-setting">
             <p className="other-setting_heading">Insurance Plans</p>
-            <div className="other-setting_section">
-                <div className="other-setting_section-first">
-                    {/* component to be used in map */}
-                    <div className="other-setting_section-first_switches-switch">
-                        <p>Select Vision Plan</p>
+           
+                {/* component to be used in map */}
+                    {
+                        getData?.length >0 && getData?.map((item)=>{
+
+                            return(
+                                <div className="other-setting_section">
+                                <div className="other-setting_section-first">
+                                <div className="other-setting_section-first_switches-switch"
+                                key={item?.id}
+                                >
+                        <p>{item?.title}</p>
                         <div>
                             <img
                                 style={{
@@ -25,15 +62,21 @@ const InsurancePlans = () => {
                                 src={bellIcon}
                                 onClick={() => {
                                     history.push({
-                                        pathname: "/edit-insurance/1",
+                                        pathname: `/edit-insurance/${item?.id}`,
                                     });
                                 }}
                             />
-                            <Switch {...label} />
+                            <Switch {...label} defaultChecked={item?.status===0?false:true ||isChecked} onChange={handleSwitch} />
                         </div>
                     </div>
-                </div>
-            </div>
+                    </div> 
+                    </div>)
+                            
+                        }) 
+                    }
+                    
+              
+          
         </div>
     );
 };
