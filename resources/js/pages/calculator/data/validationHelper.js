6e983d0d4dc80975e332bd843ccf365bc0a73379
Ali Raza <ali.raza@wadic.dev>
2022-11-01 22:43:30 +0000

    validations+invoice

--- resources/js/pages/calculator/data/validationHelper.js
+++ resources/js/pages/calculator/data/validationHelper.js
@@ -166,6 +166,18 @@ export const GetMappedPayload = (data) => {
                     copayType: data?.isCopaypremiumProgressiveAmount,
                     price: data?.copaypremiumProgressiveAmount,
                 },
+                {
+                    type: "Standard Progressives",
+                    status: data?.isCopayStandardProgressives,
+                    copayType: data?.isCopayStandardProgressiveAmount,
+                    price: data?.copayStandardProgressiveAmount,
+                },
+                {
+                    type: "Custom Progressives",
+                    status: data?.isCopayCustomProgressives,
+                    copayType: data?.isCopayCustomProgressiveAmount,
+                    price: data?.copayCustomProgressiveAmount,
+                },
             ],
         },
         lensType: {
@@ -217,6 +229,12 @@ export const mappedEditValues = (data) => {
     const premiumProgresive = lowerCopay.find(
         (item) => item.type === "Premium Progressives"
     );
+    const standardProgresive = lowerCopay.find(
+        (item) => item.type === "Standard Progressives"
+    );
+    const customProgresive = lowerCopay.find(
+        (item) => item.type === "Custom Progressives"
+    );
 
     return {
         benifitType: userState?.submitBenifitType || "",
@@ -238,6 +256,8 @@ export const mappedEditValues = (data) => {
         isCopayHighIndex: hignIndex?.status || null,
         isCopayAntiReflective: antiReflective?.status || null,
         isCopayPremiumProgressives: premiumProgresive?.status || null,
+        isCopayStandardProgressives: standardProgresive?.status || null,
+        isCopayCustomProgressives: customProgresive?.status || null,
         isCopayPolycarbonateAmount: polycarbonate?.copayType || "",
         copayPolycarbonateAmount: polycarbonate?.price || "",
         isCopayPhotochromicAmount: photochromic?.copayType || "",
@@ -246,8 +266,12 @@ export const mappedEditValues = (data) => {
         copayHighIndexAmount: hignIndex?.price || "",
         isCopayAntiReflectiveAmount: antiReflective?.copayType || "",
         copayAntiReflectiveAmount: antiReflective?.price || "",
-        isCopaypremiumProgressiveAmount: antiReflective?.copayType || "",
-        copaypremiumProgressiveAmount: antiReflective?.price || "",
+        isCopaypremiumProgressiveAmount: premiumProgresive?.copayType || "",
+        copaypremiumProgressiveAmount: premiumProgresive?.price || "",
+        isCopayStandardProgressiveAmount: standardProgresive?.copayType || "",
+        copayStandardProgressiveAmount: standardProgresive?.price || "",
+        isCopayCustomProgressiveAmount: customProgresive?.copayType || "",
+        copayCustomProgressiveAmount: customProgresive?.price || "",
         lensType: userState?.lensType?.type || "",
         lensTypeValue: userState?.lensType?.brand || "",
         lensMaterial: userState?.lensMaterial || "",
