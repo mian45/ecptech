4ee994678e0de0d7ade8c2051b453aedaf96f2ea
itwadic <saad.alvi@wadic.dev>
2022-11-05 04:35:58 +0500

    implemented antd modal in delete modal

--- resources/js/components/deleteModal/index.js
+++ resources/js/components/deleteModal/index.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import Classes from "./styles.module.scss"
 import Alert from "../../assets/Alert.png"
-import CustomCheckbox from '../customCheckbox';
+import { Col, Modal, Row } from 'antd';
 function index(props) {
+    const open=()=>{
+        console.log("open")
+    }    
     return (
-        <div className={Classes['container']} style={{height:window.innerHeight,width:window.innerWidth}}>
-            <div className={Classes['popUp']}>
+        <Modal
+        title=""
+        open={open}
+        closable={false}
+        centered={true}
+        bodyStyle={{justifyContent:"center",alignItems:"center",display:"flex"}}
+        zIndex="99999"
+        footer={null}
+      >
+            <Row justify='center'>
+            <Col sm={5}>
             <img src={Alert}style={{height:'58px',width:"58px"}}/>
+            </Col>
+            <Col sm={24}>
             <p className={Classes['confirmation-text']}>Are You Sure Delete This File ?</p>
             <p className={Classes['confirmation-subtext']}>Do you really want to delete all the data?</p>
             <div className={`${Classes['btn-container']} ${Classes['margin-container']}`}>
             <div className={Classes['btn-wrapper']}>
-                            <div className={Classes["email-setting_button-section"]}>
+            <div className={Classes["email-setting_button-section"]}>
                                 <button
                                     className={`${Classes['email-setting_button-section_save-button']} ${Classes['deactive']}`}
                                 onClick={()=>{props.cancel()}}
                                 >
                                     Cancel
                                 </button>
-                            </div>
-                        </div>
+             </div>
+             </div>
             <div className={Classes['btn-wrapper']}>
                             <div className={Classes["email-setting_button-section"]}>
                                 <button
@@ -31,9 +45,9 @@ function index(props) {
                             </div>
                         </div>
             </div>
-            
-            </div>
-        </div>
+            </Col>
+            </Row>
+            </Modal>
     );
 }
 
