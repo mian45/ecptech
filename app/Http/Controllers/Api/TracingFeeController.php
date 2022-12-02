ab1c9e363dce76f9e9c87ade89b1dc07b956633a
waseem_mushtaq <waseem.mushtaq@wadic.dev>
2022-12-02 23:59:22 +0000

    remove comments

--- app/Http/Controllers/Api/TracingFeeController.php
+++ app/Http/Controllers/Api/TracingFeeController.php
@@ -40,22 +40,6 @@ class TracingFeeController extends Controller
         return $this->sendResponse([], 'Tracing Fee not found');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -88,35 +72,6 @@ class TracingFeeController extends Controller
         return $this->sendError('Something went wrong!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request,$id)
     {
 
@@ -147,12 +102,6 @@ class TracingFeeController extends Controller
         return $this->sendError('Tracing Fee not found');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Request $request,$id)
     {
         $fee =  TracingFee::find($id);
