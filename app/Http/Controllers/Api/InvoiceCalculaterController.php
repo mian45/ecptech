9adeaf8a74d04e972f46a3a9dd889f8d58a16842
Hasnain Abid Khanzada <hasnain.abid@wadic.dev>
2022-12-05 23:41:22 +0000

    API Changes for Davis Plan

--- app/Http/Controllers/Api/InvoiceCalculaterController.php
+++ app/Http/Controllers/Api/InvoiceCalculaterController.php
@@ -138,72 +138,99 @@ class InvoiceCalculaterController extends Controller
                                 );
                             }
 
-                            if(!empty($data[3])){
-                                $collection = Collection::updateOrCreate(
-                                    ['title'=> $data[3], 'brand_id'=>$brand->id]
-                                );
-                            }
+                           
+                            if($vision_plan->title == 'Davis Vision'){
 
-                            //If vision plan is davis 
-                            //else
+                                if(!empty($data[3])){
 
-                            if(!empty($data[4])){
-                                if(strtolower($data[4]) == 'null'){
-                                    $material_id = null;
-                                }else{
-                                    $material_id = (LensMaterial::updateOrCreate(
-                                        ['lens_material_title'=> $data[4]]
-                                    ))->id;
-                                }
-
-
-                            }
-
-                            if(!empty($data[5])){
-                                $lense = Lense::create(
-                                    ['title'=> $data[5], 'collection_id'=>$collection->id, "lens_material_id"=>$material_id]
-                                );
-                            }
-
-                            if(!empty($data[6])){
-
-                                $code_id = null;
-                                if(!empty($data[7])){
-
-                                    if(!empty($data[1])){
-                                        if(strtolower($data[1]) == 'bifocal' OR strtolower($data[1]) == 'trifocal' ){
-                                            $check_lensetype = "biofocal";
-                                        }else{
-                                            $check_lensetype = "other";
+                                    if($lens_type->title==='PAL'){
+                                        if(!empty($data[4])){
+                                            $category = $data[4];
                                         }
-                                    } 
-                                    
-                                    if($check_lensetype == 'biofocal'){
-                                        $code = Code::where('name',$data[7])->where('vision_plan_id',$vision_plan->id)->where('lense_type','bifocal')->first();
-                                    }
-                                    else{
-                                        $code = Code::where('name',$data[7])->where('vision_plan_id',$vision_plan->id)->whereNull('lense_type')->first();
+                                        $code = Code::where('vision_plan_id', $vision_plan->id)
+                                        ->where('lense_type_id', $lens_type->id)
+                                        ->where('category',$category)
+                                        ->first();
+                                    }else{
+                                        $category = NULL;
+                                        $code = Code::where('vision_plan_id', $vision_plan->id)->where('lense_type_id', $lens_type->id)->first();
                                     }
 
-                                    if($code){
-                                        $code_id = $code->id;
-                                    }
+
+                                    $collection = Collection::updateOrCreate(
+                                        ['title'=> $data[3], 'brand_id'=>$brand->id, 'code_id'=>$code->id],
+                                        ['category'=>$category]
+                                    );
                                 }
 
-                                $type = null;
-                                if(!empty($data[8])){
-                                    $type = strtolower($data[8]);
+                            }else{
+
+                                if(!empty($data[3])){
+                                    $collection = Collection::updateOrCreate(
+                                        ['title'=> $data[3], 'brand_id'=>$brand->id]
+                                    );
                                 }
-                                $characteristic = Characteristic::create(
-                                    ['title'=> $data[6], 'lense_id'=>$lense->id, "code_id"=>$code_id, "type"=>$type]
-                                );
 
-                                if(!empty($data[9])){
-                                    $category = $data[9];
-                                    $collection_obj = Collection::find($collection->id);
-                                    $collection_obj->category = $category;
-                                    $collection_obj->save();
+                                
 
+                                if(!empty($data[4])){
+                                    if(strtolower($data[4]) == 'null'){
+                                        $material_id = null;
+                                    }else{
+                                        $material_id = (LensMaterial::updateOrCreate(
+                                            ['lens_material_title'=> $data[4]]
+                                        ))->id;
+                                    }
+
+
+                                }
+
+                                if(!empty($data[5])){
+                                    $lense = Lense::create(
+                                        ['title'=> $data[5], 'collection_id'=>$collection->id, "lens_material_id"=>$material_id]
+                                    );
+                                }
+
+                                if(!empty($data[6])){
+
+                                    $code_id = null;
+                                    if(!empty($data[7])){
+
+                                        if(!empty($data[1])){
+                                            if(strtolower($data[1]) == 'bifocal' OR strtolower($data[1]) == 'trifocal' ){
+                                                $check_lensetype = "biofocal";
+                                            }else{
+                                                $check_lensetype = "other";
+                                            }
+                                        } 
+                                        
+                                        if($check_lensetype == 'biofocal'){
+                                            $code = Code::where('name',$data[7])->where('vision_plan_id',$vision_plan->id)->where('lense_type','bifocal')->first();
+                                        }
+                                        else{
+                                            $code = Code::where('name',$data[7])->where('vision_plan_id',$vision_plan->id)->whereNull('lense_type')->first();
+                                        }
+
+                                        if($code){
+                                            $code_id = $code->id;
+                                        }
+                                    }
+
+                                    $type = null;
+                                    if(!empty($data[8])){
+                                        $type = strtolower($data[8]);
+                                    }
+                                    $characteristic = Characteristic::create(
+                                        ['title'=> $data[6], 'lense_id'=>$lense->id, "code_id"=>$code_id, "type"=>$type]
+                                    );
+
+                                    if(!empty($data[9])){
+                                        $category = $data[9];
+                                        $collection_obj = Collection::find($collection->id);
+                                        $collection_obj->category = $category;
+                                        $collection_obj->save();
+
+                                    }
                                 }
                             }
 
@@ -328,46 +355,73 @@ class InvoiceCalculaterController extends Controller
                                 $vision_plan = VisionPlan::where('title', $data[0])->first();
                             }
 
-                            //If vision plan is davis then
-                            //else
+                            if($vision_plan->title == 'Davis Vision'){
 
-                            if(!empty($data[1])){
-                                $price = str_replace('$','',$data[2]);
-                                $code = str_replace(' ', '', $data[1]);
+                                if(!empty($data[1])){
+                                    $lense_type = LenseType::updateOrCreate(
+                                        ['title'=> $data[1], 'vision_plan_id'=>$vision_plan->id]
+                                    );
+                                }
 
-                                if(is_numeric($price)){
+                                if(!empty($data[2])){
+                                    $price = moneyFormatter($data[2]);
+                                }
 
-                                    $price = (float)$price;
+                                if($lense_type->title =='PAL'){
+
                                     $code = Code::updateOrCreate(
-                                        ['vision_plan_id'=>$vision_plan->id,'lense_type'=>NULL,'name'=>$code],
+                                        ['vision_plan_id'=>$vision_plan->id,'lense_type_id'=>$lense_type->id, 'category'=>$data[3]],
                                         ['price'=> $price]
                                     );
 
                                 }else{
                                     $code = Code::updateOrCreate(
-                                        ['vision_plan_id'=>$vision_plan->id,'lense_type'=>NULL,'name'=>$code],
-                                        ['price_formula'=> $price]
+                                        ['vision_plan_id'=>$vision_plan->id,'lense_type_id'=>$lense_type->id],
+                                        ['price'=> $price]
                                     );
                                 }
+    
+
+                            }else{
+
+                                if(!empty($data[1])){
+                                    $price = str_replace('$','',$data[2]);
+                                    $code = str_replace(' ', '', $data[1]);
+
+                                    if(is_numeric($price)){
+
+                                        $price = (float)$price;
+                                        $code = Code::updateOrCreate(
+                                            ['vision_plan_id'=>$vision_plan->id,'lense_type'=>NULL,'name'=>$code],
+                                            ['price'=> $price]
+                                        );
+
+                                    }else{
+                                        $code = Code::updateOrCreate(
+                                            ['vision_plan_id'=>$vision_plan->id,'lense_type'=>NULL,'name'=>$code],
+                                            ['price_formula'=> $price]
+                                        );
+                                    }
 
 
-                                $price_bifocal = str_replace('$','',$data[3]);
-                                $code_biofocal = str_replace(' ', '', $data[1]);
-                                if(is_numeric($price_bifocal)){
+                                    $price_bifocal = str_replace('$','',$data[3]);
+                                    $code_biofocal = str_replace(' ', '', $data[1]);
+                                    if(is_numeric($price_bifocal)){
 
-                                    $price_bifocal = (float)$price_bifocal;
-                                    $code = Code::updateOrCreate(
-                                        ['vision_plan_id'=>$vision_plan->id,'lense_type'=>'bifocal','name'=>$code_biofocal],
-                                        ['price'=> $price_bifocal]
-                                    );
+                                        $price_bifocal = (float)$price_bifocal;
+                                        $code = Code::updateOrCreate(
+                                            ['vision_plan_id'=>$vision_plan->id,'lense_type'=>'bifocal','name'=>$code_biofocal],
+                                            ['price'=> $price_bifocal]
+                                        );
 
-                                }else{
-                                    $code = Code::updateOrCreate(
-                                        ['vision_plan_id'=>$vision_plan->id,'lense_type'=>'bifocal','name'=>$code_biofocal],
-                                        ['price_formula'=> $price_bifocal]
-                                    );
+                                    }else{
+                                        $code = Code::updateOrCreate(
+                                            ['vision_plan_id'=>$vision_plan->id,'lense_type'=>'bifocal','name'=>$code_biofocal],
+                                            ['price_formula'=> $price_bifocal]
+                                        );
+                                    }
+
                                 }
-
                             }
                         }
 
@@ -387,6 +441,70 @@ class InvoiceCalculaterController extends Controller
 
 
     }
+
+
+    public function storeLenseMaterialCSVData(Request $request){
+        $validator = Validator::make($request->all(), [
+            'csv' => 'required|mimes:csv,txt'
+        ]);
+        if ($validator->fails()) {
+            throw (new ValidationException($validator));
+        }
+
+        $csv = array();
+
+        if($_FILES['csv']['error'] == 0){
+            $tmpName = $_FILES['csv']['tmp_name'];
+
+            if(($handle = fopen($tmpName, 'r')) !== FALSE) {
+
+                DB::beginTransaction();
+                $row = 0;
+                try{
+                    while(($data = fgetcsv($handle, 1000, ',')) !== FALSE) {
+                        if($row==0){
+                            // number of fields in the csv
+                            $col_count = count($data);
+                        }else{
+                            $data = $this->clear_encoding_str($data);
+                            if(!empty($data[0])){
+                                $vision_plan = VisionPlan::where('title', $data[0])->first();
+                            }
+
+                            if(!empty($data[1])){
+                                $lense_material = LensMaterial::where('lens_material_title', $data[1])->first();
+                            }
+
+                            if(!empty($data[2])){
+                                $price = moneyFormatter($data[2]);
+                            }
+
+                            $code = Code::updateOrCreate(
+                                ['vision_plan_id'=>$vision_plan->id,'lense_material_id'=>$lense_material->id],
+                                ['price'=> $price]
+                            );
+
+                        }
+
+                        $row++;
+                    }
+                    fclose($handle);
+                    DB::commit();
+                    return $this->sendResponse([], 'Lense Material Price CSV data uploaded successfully');
+                }catch(\Exception $e){
+                    DB::rollback();
+                    return $this->sendError($e->getMessage());
+
+                }
+
+            }
+        }
+
+
+    }
+
+
+
 
 
 
