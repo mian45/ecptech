38078fa0be234a1e54c0a054fe456a78a3cf862e
safeerbaig <safeer.baig@wadic.dev>
2022-12-07 18:24:00 +0000

    fix: commit

--- resources/js/components/invoiceForm/index.js
+++ resources/js/components/invoiceForm/index.js
@@ -93,7 +93,15 @@ const InvoicesForm = ({
                 <ButtonComponent
                     className={classes["search-button"]}
                     type={"button"}
-                    // disabled={!(isValid && dirty) || isSubmitting || isSearched}
+                    disabled={
+                        !(
+                            values.firstName ||
+                            values.lastName ||
+                            values.dob ||
+                            values.phoneNo ||
+                            values.email
+                        )
+                    }
                     onClick={() => handleSearch(values)}
                 >
                     Search Invoices
