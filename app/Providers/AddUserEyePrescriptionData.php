9bfa340dcf7829f063f5c5ce6bcdb9a32575274d
waseem_mushtaq <waseem.mushtaq@wadic.dev>
2022-12-08 22:54:45 +0000

    add prescription

--- app/Providers/AddUserEyePrescriptionData.php
+++ app/Providers/AddUserEyePrescriptionData.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use App\Providers\UserEyePrescriptionData;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
-use App\Models\Prescription;
+use Illuminate\Support\Facades\DB;
 class AddUserEyePrescriptionData
 {
     /**
@@ -26,40 +26,64 @@ class AddUserEyePrescriptionData
      */
     public function handle(UserEyePrescriptionData $event)
     {
-
-        
         $user = $event->user;
-        
-        $data = array(
-            [
-                "name" => "Trivex",
-                "sphere_from" => "",
-                "sphere_to" => "",
-            ],
-            [
-                "name" => "Hi Index 1.67",
-                "sphere_from" => "",
-                "sphere_to" => "",
-            ],
-            [
-                "name" => "Hi Index 1.70",
-                "sphere_from" => "",
-                "sphere_to" => "",
-            ]
-       
-        );
-        
-        
-            
+        DB::table('prescriptions')->insert(array(
+            array(
 
-        // 'name' => fake()->name(),
-        // 'sphere_from' => fake()->randomDigit,            
-        // 'sphere_to' => fake()->randomDigit,
-        // 'cylinder_from' => fake()->randomDigit,
-        // 'cylinder_to' => fake()->randomDigit,
-        // 'user_id' => User::all()->random()->id,
-        // 'created_at' => date("Y-m-d H:i:s"),
-        // 'updated_at' => date("Y-m-d H:i:s"),
-        // 'created_by' => User::all()->random()->id,
+                'name' => "Hi index 1.70",
+                'sphere_from' => 5,
+                'sphere_to' => 20,
+                'user_id' => $user->id,
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ),
+            array(
+
+                'name' => "Hi index 1.70",
+                'sphere_from' => -5,
+                'sphere_to' => -20,
+                'user_id' => $user->id,
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ),
+            array(
+
+                'name' => "Hi Index 1.67",
+                'sphere_from' => -4.25,
+                'sphere_to' => 4.25,
+                'user_id' => $user->id,
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ),
+            array(
+
+                'name' => "Hi Index 1.67",
+                'sphere_from' => -4.50,
+                'sphere_to' => 4.50,
+                'user_id' => $user->id,
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ),
+            array(
+
+                'name' => "Hi Index 1.67",
+                'sphere_from' => -4.75,
+                'sphere_to' => 4.75,
+                'user_id' => $user->id,
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ),
+            array(
+
+                'name' => "Trivex",
+                'sphere_from' => -4,
+                'sphere_to' => 4,
+                'user_id' => $user->id,
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            )
+            
+        ));
+      
     }
 }
