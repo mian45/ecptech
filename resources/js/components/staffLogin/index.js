0c66c57257eeada57e35bcc9b18e28be8e57de42
safeerbaig <safeer.baig@wadic.dev>
2022-12-09 19:54:32 +0000

    fix: ECP-371

--- resources/js/components/staffLogin/index.js
+++ resources/js/components/staffLogin/index.js
@@ -173,7 +173,7 @@ const StaffLogin = ({ dispatch, clientUser, staffUser }) => {
                                 <Col
                                     className={classes["input-wrapper"]}
                                     xs={24}
-                                    md={11}
+                                    md={12}
                                 >
                                     <div className={classes["subtitle"]}>
                                         Password
@@ -202,7 +202,7 @@ const StaffLogin = ({ dispatch, clientUser, staffUser }) => {
                                 <Col
                                     className={classes["input-wrapper"]}
                                     xs={24}
-                                    md={11}
+                                    md={12}
                                 >
                                     <ErrorMessage
                                         name="email"
@@ -213,7 +213,7 @@ const StaffLogin = ({ dispatch, clientUser, staffUser }) => {
                                 <Col
                                     className={classes["input-wrapper"]}
                                     xs={24}
-                                    md={11}
+                                    md={12}
                                 >
                                     <ErrorMessage
                                         name="password"
@@ -226,7 +226,7 @@ const StaffLogin = ({ dispatch, clientUser, staffUser }) => {
                                 <Col
                                     className={`${classes["input-wrapper"]} ${classes["margin-top-container"]}`}
                                     xs={24}
-                                    md={11}
+                                    md={12}
                                 >
                                     <div className={classes["subtitle"]}>
                                         Confirm Password
@@ -247,7 +247,7 @@ const StaffLogin = ({ dispatch, clientUser, staffUser }) => {
                                 <Col
                                     className={`${classes["input-wrapper"]} ${classes["margin-top-container"]}`}
                                     xs={24}
-                                    md={11}
+                                    md={12}
                                 >
                                     <ErrorMessage
                                         name="confirmPassword"
