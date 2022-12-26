00059bd474f480147e81979a494119ab92f3226b
waseem_mushtaq <waseem.mushtaq@wadic.dev>
2022-12-26 18:51:10 +0000

    email design task

---
+++ resources/views/emails/invoice.blade.php
@@ -0,0 +1,324 @@
+<!doctype html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title></title>
+    <style>
+      
+      
+      @font-face {
+       font-family: 'Montserrat';
+       font-style: normal;
+       font-display: swap;
+       src: url(https://fonts.gstatic.com/s/montserrat/v25/JTUSjIg1_i6t8kCHKm459Wdhyzbi.woff2) format('woff2');
+         unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+      }
+      
+      /*All the styling goes here*/
+      
+      img {
+        border: none;
+        -ms-interpolation-mode: bicubic;
+        max-width: 100%; 
+      }
+
+      body {
+        background-color: #f6f6f6;
+        font-family: 'Montserrat';
+        -webkit-font-smoothing: antialiased;
+        font-size: 14px;
+        line-height: 1.4;
+        margin: 0;
+        padding: 0;
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%; 
+      }
+
+      table {
+        border-collapse: separate;
+        mso-table-lspace: 0pt;
+        mso-table-rspace: 0pt;
+        width: 100%; }
+        table td {
+          font-family: 'Montserrat';
+          font-size: 14px;
+          vertical-align: top; 
+      }
+
+      /* -------------------------------------
+          BODY & CONTAINER
+      ------------------------------------- */
+
+      .body {
+        font-family: 'Montserrat';
+        color: #2A2A2A;
+        background-color: #f6f6f6;
+        width: 100%; 
+      }
+
+      /* Set a max-width, and make it display as block so it will automatically stretch to that width, but will also shrink down on a phone or something */
+      .container {
+        display: block;
+        margin: 0 auto !important;
+        /* makes it centered */
+        max-width: 671px;
+        padding: 10px;
+        width: 671px; 
+      }
+
+      /* This should also be a block element, so that it will fill 100% of the .container */
+      .content {
+        box-sizing: border-box;
+        display: block;
+        margin: 0 auto;
+        max-width: 671px;
+        padding: 10px; 
+      }
+
+      /* -------------------------------------
+          HEADER, FOOTER, MAIN
+      ------------------------------------- */
+      .main {
+        background: #ffffff;
+        border-radius: 3px;
+        width: 100%; 
+      }
+
+      .wrapper {
+        box-sizing: border-box;
+        padding: 20px; 
+      }
+
+      .content-block {
+        padding-bottom: 10px;
+        padding-top: 10px;
+      }
+
+      .footer {
+        clear: both;
+        margin-top: 10px;
+        text-align: center;
+        width: 100%; 
+      }
+        .footer td,
+        .footer p,
+        .footer span,
+        .footer a {
+          color: #999999;
+          font-size: 12px;
+          text-align: center; 
+      }
+
+      /* -------------------------------------
+          TYPOGRAPHY
+      ------------------------------------- */
+      
+      h2{
+        font-weight: bold;
+        font-size: 18px;
+      }
+      strong{
+        font-weight: bold;
+        font-size: 14px;
+        line-height: 18px;
+        letter-spacing: 0px;
+        opacity: 1;
+      }
+      table.body tr td p{
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 15px;
+        letter-spacing: 0px;
+        opacity: 1;
+      }
+      table.body tr td p span{
+        font-size: 14px;
+        font-weight: 400;
+      }
+      table.body tr{
+        line-height: 15px;
+      }
+      /* -------------------------------------
+          RESPONSIVE AND MOBILE FRIENDLY STYLES
+      ------------------------------------- */
+      @media only screen and (max-width: 620px) {
+    
+      }
+
+     
+
+      
+    </style>
+  </head>
+  <body>
+  
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body">
+      <tr>
+        <td>&nbsp;</td>
+        <td class="container">
+          <div class="content">
+
+            <!-- START CENTERED WHITE CONTAINER -->
+            <table role="presentation" class="main">
+
+              <!-- START MAIN CONTENT AREA -->
+              <tr>
+                <td class="wrapper">
+                  <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                    <tr>
+                      <td>
+                        <h2>Estimate for John Varvatos Sunglasses</h2>
+                        <p><strong>Name:</strong> <span> John Smith<span>&nbsp;&nbsp;<strong>Email:</strong> <span> john@mymail.com<span>&nbsp;&nbsp;<strong>Phone:</strong> <span> 123 456 7890<span></p>
+                        <hr>
+                        <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+                          <tbody>
+                            <tr>
+                              <td align="left">
+                                 <strong>Item Description</strong>
+                              </td>
+                              <td align="right">
+                                 <strong>Price</strong>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td align="left">
+                                 <p>Frame Order</p>
+                              </td>
+                              <td align="right">
+                                 <p>$0.00</p>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td align="left">
+                                 <p>Frame</p>
+                              </td>
+                              <td align="right">
+                                 <p>$0.00</p>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td align="left">
+                                 <p>Drill Mounte</p>
+                              </td>
+                              <td align="right">
+                                 <p>$0.00</p>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td align="left">
+                                 <p>Lenses</p>
+                              </td>
+                              <td align="right">
+                                 <p>$445.54</p>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td align="left">
+                                 <p>Shamir Computer/Workspace ( Base fee )</p>
+                              </td>
+                              <td align="right">
+                                 <p>$50.00</p>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td align="left">
+                                 <p>Shamir Computer/Workspace ( Lens Material: Polycarbonate )</p>
+                              </td>
+                              <td align="right">
+                                 <p>$0.00</p>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td align="left">
+                                 <p>Material Copay</p>
+                              </td>
+                              <td align="right">
+                                 <p>$200.00</p>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td align="left">
+                                 <p>Photochromic Option: Transitions Signature 8</p>
+                              </td>
+                              <td align="right">
+                                 <p>$75.00</p>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td align="left">
+                                 <p>Antireflective Properties: Shamir Glacier Plus UV</p>
+                              </td>
+                              <td align="right">
+                                 <p>$85.00</p>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td align="left">
+                                 <p>Mirror Coating: Ski Type Mirror </p>
+                              </td>
+                              <td align="right">
+                                 <p>$47.20</p>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td align="left">
+                                 <p>Is Sunglass Lens Polarized?</p>
+                              </td>
+                              <td align="right">
+                                 <p>Yes</p>
+                              </td>
+                            </tr>
+                            
+                          </tbody>
+                        </table>
+                        <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                        <tbody>
+                           <tr>
+                              <td align="left">
+                                <ul>
+                                  <li>Polycarbonate lenses are thinner, lighter weight, and impact resistant space-age lenses. Single vision.</li>
+                                </ul>
+                              </td>
+                            </tr>
+                            
+                          </tbody>
+                        </table>
+                        <p>This is a really simple email template. Its sole purpose is to get the recipient to click the button with no distractions.</p>
+                        <p>Good luck! Hope it works.</p>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+
+            <!-- END MAIN CONTENT AREA -->
+            </table>
+            <!-- END CENTERED WHITE CONTAINER -->
+
+            <!-- START FOOTER -->
+            <div class="footer">
+              <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td class="content-block">
+                    <span class="apple-link">Company Inc, 3 Abbey Road, San Francisco CA 94102</span>
+                    <br> Don't like these emails? <a href="http://i.imgur.com/CScmqnj.gif">Unsubscribe</a>.
+                  </td>
+                </tr>
+                <tr>
+                  <td class="content-block powered-by">
+                    Powered by <a href="http://htmlemail.io">HTMLemail</a>.
+                  </td>
+                </tr>
+              </table>
+            </div>
+            <!-- END FOOTER -->
+
+          </div>
+        </td>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
+  </body>
+</html>
